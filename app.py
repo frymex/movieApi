f5755cb827e5729762e6a46ff96e72768308d467
frymex <t_frymex@protonmail.com>
2022-08-10 18:27:00 +0300

    update example file

--- app.py
+++ app.py
@@ -2,4 +2,20 @@ from api.api import MovieAPI
 
 app = MovieAPI([])
 
-x = (app.get_info_by_id(842114))
+kinopoisk_id = 1
+query = ''
+
+
+def get_player() -> dict:
+    var = app.get_by_kinopoisk(kinopoisk_id)
+    return var
+
+
+def get_infos() -> dict:
+    var = app.get_info_by_id(kinopoisk_id)
+    return var.as_dict()
+
+
+def search_by_query() -> dict:
+    var = app.search_kinopoisk(query)
+    return var
